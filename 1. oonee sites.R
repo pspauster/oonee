34c8b627c38f8f1f72cc5d0559bc7cf8e259fbf8
Patrick Spauster <patrick.spauster@gmail.com>
2023-03-20 23:36:27 -0400

    updated annotations

--- 1. oonee sites.R
+++ 1. oonee sites.R
@@ -2,6 +2,12 @@ library(tidyverse)
 library(janitor)
 library(sf)
 
+#start off by making your own script and saving it with a number and the name of the data you're reading in
+#the goal of this is to read in data, clean it, put it in spatial format, and save it to the clean data folder
+
+#when you're done, you're going to want to "push" your changes to github so we all see the data
+
+#start by dropping the raw data in the "raw" folder on your machine
 #read in data
 pod_locations <- read_csv("Data/raw/ooneepods - ooneepods.csv") 
 
